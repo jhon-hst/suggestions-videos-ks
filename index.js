c32b1ea96760378d39f199d308f01d1eca5fed2d
Jhon Sanchez <jhonhst2@gmail.com>
2023-01-15 16:31:24 -0500

    added new videos

---
+++ index.js
@@ -0,0 +1,19 @@
+const myYoutubeVideos = require('./videos.json')
+
+const parseVideos = myYoutubeVideos.map((video) => ({
+    name: video.title,
+    url: `https://www.youtube.com/watch?v=${video.videoId}`,
+    thumbnail: `https://i.ytimg.com/vi/${video.videoId}/maxresdefault.jpg`,
+    type: "video"
+}))
+
+let groups = [];
+for (let i = 0; i < parseVideos.length; i += 6) {
+  let group = {
+    title: "#" + (i/6 + 1),
+    items: [...parseVideos.slice(i, i + 10),  {type: "message"}]
+  };
+  groups.push(group);
+}
+
+console.log(JSON.stringify(groups))
